8e94b6d7e7315b0e6840b15474cd0ae4942c2359
Ner'zhul <nerzhultheking@hotmail.fr>
2010-04-22 16:07:09 +0200

    [FX-63] resilience final fix & crashfix on violet hold
    Signed-off-by: Ner'zhul <nerzhultheking@hotmail.fr>

--- src/bindings/ScriptDev2/scripts/northrend/violet_hold/instance_violethold.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/violet_hold/instance_violethold.cpp
@@ -438,8 +438,11 @@ struct MANGOS_DLL_DECL instance_violethold : public ScriptedInstance
 
 	void ClosePortal(uint32 id_portal)
 	{
-		portals[id_portal]->DealDamage(portals[id_portal],portals[id_portal]->GetMaxHealth(),NULL, DIRECT_DAMAGE, SPELL_SCHOOL_MASK_NORMAL, NULL, false);
-		portals[id_portal]->ForcedDespawn();
+		if(portals[id_portal] && portals[id_portal]->isAlive())
+		{
+			portals[id_portal]->DealDamage(portals[id_portal],portals[id_portal]->GetMaxHealth(),NULL, DIRECT_DAMAGE, SPELL_SCHOOL_MASK_NORMAL, NULL, false);
+			portals[id_portal]->ForcedDespawn();
+		}
 		portals[id_portal] = NULL;
 		portal_opened[id_portal] = false;
 	}
