4c9d2bd99d59f15a01ea49addbe1a7e2701fb76d
Ner'zhul <nerzhultheking@gmail.com>
2010-04-25 23:57:53 +0200

    compile fixes
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -5471,7 +5471,8 @@ void Spell::EffectInterruptCast(uint32 i)
             // check if we can interrupt spell
             if ((curSpellInfo->InterruptFlags & SPELL_INTERRUPT_FLAG_INTERRUPT) && curSpellInfo->PreventionType == SPELL_PREVENTION_TYPE_SILENCE )
             {
-                unitTarget->ProhibitSpellSchool(GetSpellSchoolMask(curSpellInfo), unitTarget->CalculateSpellDuration(m_spellInfo, i, unitTarget));
+				uint32 zero = 0;
+                unitTarget->ProhibitSpellSchool(GetSpellSchoolMask(curSpellInfo), unitTarget->CalculateSpellDuration(m_spellInfo, i, unitTarget,zero));
                 unitTarget->InterruptSpell(CurrentSpellTypes(i),false);
             }
         }
--- src/game/Unit.h
+++ src/game/Unit.h
@@ -1748,6 +1748,7 @@ class MANGOS_DLL_SPEC Unit : public WorldObject
 
         uint32 CalcArmorReducedDamage(Unit* pVictim, const uint32 damage);
         void CalcAbsorbResist(Unit *pVictim, SpellSchoolMask schoolMask, DamageEffectType damagetype, const uint32 damage, uint32 *absorb, uint32 *resist, bool canReflect = false);
+		void CalculateHealAbsorb(Unit *pVictim, const SpellEntry *spellProto, uint32 &HealAmount, uint32 &Absorbed);
 
         void  UpdateWalkMode(Unit* source, bool self = true);
         void  UpdateSpeed(UnitMoveType mtype, bool forced);
