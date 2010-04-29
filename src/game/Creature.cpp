fe3d564b193876eff8d601452a94513ada9871ed
Ner'zhul <nerzhultheking@hotmail.fr>
2010-04-29 16:04:18 +0200

    [FX-124] new test on vehicles
    Signed-off-by: Ner'zhul <nerzhultheking@hotmail.fr>

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -1064,6 +1064,9 @@ bool Creature::CreateFromProto(uint32 guidlow, uint32 Entry, uint32 team, const 
 	if(!vehId)
 		vehId = cinfo->VehicleId;
 
+	if(vehId)
+		sLog.outError("Create vehicle %u entry %u",vehId,Entry);
+		
 	if(vehId && !CreateVehicleKit(vehId))
 		vehId = 0;
 
