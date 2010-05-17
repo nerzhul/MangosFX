78e6de43c916a428d6d6e36076f3665143e86630
Ner'zhul <nerzhultheking@hotmail.fr>
2010-05-17 15:31:52 +0200

    [FX-269] Cold Flame must is mathematicly spawned :) Ty to Lucifuru
    Signed-off-by: Ner'zhul <nerzhultheking@hotmail.fr>

--- src/bindings/ScriptDev2/scripts/northrend/icecrown_citadel/icecrown_raid/boss_lord_marrowgar.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/icecrown_citadel/icecrown_raid/boss_lord_marrowgar.cpp
@@ -3,15 +3,15 @@
 
 enum Spells
 {
-    SPELL_SABER_LASH     		= 69055,
-    SPELL_COLDFLAME      		= 69146,
-    SPELL_BONE_SPIKE     		= 69057,
-    SPELL_BONE_STRIKE_IMPALE	= 69065,
-    SPELL_BONE_STORM     		= 69076,
+    SPELL_SABER_LASH     		= 69055, // ok
+    SPELL_COLDFLAME      		= 69146, // ok
+    SPELL_BONE_SPIKE     		= 69057, 
+    SPELL_BONE_SPIKE_IMPALE		= 69065, 
+    SPELL_BONE_STORM     		= 69076, // ok
     
     SPELL_BERSERK               = 47008,
     
-    NPC_COLDFLAME        		= 36672, // 3 sec of spawn in normal but 8 in heroic
+    NPC_COLDFLAME        		= 36672,
     NPC_BONE_SPIKE              = 38711,
 };
 
@@ -41,16 +41,21 @@ struct MANGOS_DLL_DECL boss_marrowgarAI : public LibDevFSAI
 				FlameDespawn = 3000;
 				break;
 		}
+		AddEventOnTank(SPELL_SABER_LASH,1000,1000);
     }
 	
 	uint8 phase;
 	uint32 FlameDespawn;
 	uint32 Flame_Timer;
+	uint32 Storm_Timer;
+	uint32 StormTarget_Timer;
 
     void Reset()
     {
 		ResetTimers();
 		Flame_Timer = 12000;
+		Storm_Timer = 27000;
+		StormTarget_Timer = 15000;
 		phase = 0;
     }
 
@@ -72,32 +77,76 @@ struct MANGOS_DLL_DECL boss_marrowgarAI : public LibDevFSAI
             pInstance->SetData(TYPE_MARROWGAR, FAIL);
     }
 
+
 	void CallColdFlames()
 	{
 		Unit* flameTarget = GetRandomUnit();
 		if(!flameTarget)
 			return;
 		
-		float x_vect = flameTarget->GetPositionX() - me->GetPositionX();
-		float y_vect = flameTarget->GetPositionY() - me->GetPositionY();
-		for(uint8 i=0;i<5;i++)
-			CallCreature(NPC_COLDFLAME,FlameDespawn,PREC_COORDS,NOTHING,me->GetPositionX() + x_vect*(3*i),me->GetPositionY() + x_vect*(3*i),me->GetPositionZ() + 1.0f,true);
+		float dist = me->GetDistance2d(flameTarget);
+		uint8 nbFlame = ceil((double)dist/3.0f) + 1;
+
+		for(uint8 i=0;i<(nbFlame+1);i++)
+		{
+			float x_add = i*3*dist/(flameTarget->GetPositionX() - me->GetPositionX());
+			float y_add = i*3*dist/(flameTarget->GetPositionY() - me->GetPositionY());
+			CallCreature(NPC_COLDFLAME,FlameDespawn,PREC_COORDS,NOTHING,me->GetPositionX() + x_add, me->GetPositionY() + y_add, me->GetPositionZ() + 1.0f,true);
+		}
 	}
 	
     void UpdateAI(const uint32 diff)
     {
         if (!CanDoSomething())
             return;
-            
+        
         if(Flame_Timer <= diff)
-        {
+		{
 			CallColdFlames();
 			Flame_Timer = 3000;
 		}
 		else
 			Flame_Timer -= diff;
-
-		UpdateEvent(diff);
+				
+        if(phase == 0)
+        {
+			if(Storm_Timer <= diff)
+			{
+				phase = 1;
+				me->CastStop();
+				DoCastMe(SPELL_BONE_STORM);
+				Speak(CHAT_TYPE_BOSS_EMOTE,0,"Gargamoelle commence a incanter une tempete d'os");
+				Storm_Timer = 60000;
+				StormTarget_Timer = 15000;
+			}
+			else
+				Storm_Timer -= diff;
+				
+			UpdateEvent(diff);
+		}
+		else
+		{
+			if(StormTarget_Timer <= diff)
+			{
+				DoResetThreat();
+				Unit* StormTarget = GetRandomUnit();
+				if(StormTarget)
+					me->AddThreat(StormTarget,100000.0f);
+				StormTarget_Timer = 15000;
+			}
+			else
+				StormTarget_Timer -= diff;
+				
+			if(Storm_Timer <= diff)
+			{
+				phase = 0;
+				DoResetThreat();
+				Storm_Timer = 27000;
+			}
+			else
+				Storm_Timer -= diff;
+		}
+		
 
         DoMeleeAttackIfReady();
     }
