9120cedbaae95edfb0fcb4471e8241bcd677427c
Ner'zhul <nerzhultheking@gmail.com>
2010-06-03 04:23:12 +0200

    [FX-458] Typo fix
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/game/Level0.cpp
+++ src/game/Level0.cpp
@@ -93,8 +93,8 @@ bool ChatHandler::HandleServerInfoCommand(const char* /*args*/)
     uint32 maxQueuedClientsNum = sWorld.GetMaxQueuedSessionCount();
     std::string str = secsToTimeString(sWorld.GetUptime());
 
-    PSendSysMessage("MangosFX rev 305 based on Mangos 9313 developped by Ner'zhul");
-    PSendSysMessage(LANG_USING_SCRIPT_LIB,"LibDevFS rev 93, EDC 2.3, ICC 0.3");
+    PSendSysMessage("MangosFX rev 448 based on Mangos 9313 developped by Ner'zhul");
+    PSendSysMessage(LANG_USING_SCRIPT_LIB,"LibDevFS rev 101, EDC 2.3, ICC 0.4");
     PSendSysMessage(LANG_CONNECTED_USERS, activeClientsNum, maxActiveClientsNum, queuedClientsNum, maxQueuedClientsNum);
     PSendSysMessage(LANG_UPTIME, str.c_str());
 
