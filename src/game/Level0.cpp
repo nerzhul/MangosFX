88051b70a102d6126906526cac1f59013ddd145e
Ner'zhul <nerzhultheking@gmail.com>
2010-06-09 00:13:51 +0200

    [FX-543] Revision fix :D
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/game/Level0.cpp
+++ src/game/Level0.cpp
@@ -93,7 +93,7 @@ bool ChatHandler::HandleServerInfoCommand(const char* /*args*/)
     uint32 maxQueuedClientsNum = sWorld.GetMaxQueuedSessionCount();
     std::string str = secsToTimeString(sWorld.GetUptime());
 
-    PSendSysMessage("MangosFX rev 448 based on Mangos 9313 developped by Ner'zhul");
+    PSendSysMessage("MangosFX rev 542 based on Mangos 9313 developped by Ner'zhul");
     PSendSysMessage(LANG_USING_SCRIPT_LIB,"LibDevFS rev 101, EDC 2.3, ICC 0.4");
     PSendSysMessage(LANG_CONNECTED_USERS, activeClientsNum, maxActiveClientsNum, queuedClientsNum, maxQueuedClientsNum);
     PSendSysMessage(LANG_UPTIME, str.c_str());
