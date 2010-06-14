51f98fd246340dbda2bb6196399ead617d533dd6
Ner'zhul <nerzhultheking@gmail.com>
2010-06-14 12:02:04 +0200

    [FX-581] Ring of valor basic implementation
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/game/BattleGroundMgr.cpp
+++ src/game/BattleGroundMgr.cpp
@@ -1557,8 +1557,8 @@ BattleGround * BattleGroundMgr::CreateNewBattleGround(BattleGroundTypeId bgTypeI
     //for arenas there is random map used
     if (bg_template->isArena())
     {
-        BattleGroundTypeId arenas[] = {BATTLEGROUND_NA, BATTLEGROUND_BE, BATTLEGROUND_RL, BATTLEGROUND_DS, BATTLEGROUND_RV};
-        uint32 arena_num = urand(0,2);
+        BattleGroundTypeId arenas[] = {BATTLEGROUND_NA, BATTLEGROUND_BE, BATTLEGROUND_RL, BATTLEGROUND_RV, BATTLEGROUND_DS};
+        uint32 arena_num = urand(0,3);
         bgTypeId = arenas[arena_num];
         bg_template = GetBattleGroundTemplate(bgTypeId);
         if (!bg_template)
