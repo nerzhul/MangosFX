fd70d865a808a6b844849f60adfc20e630e4d232
Ner'zhul <nerzhultheking@gmail.com>
2010-06-20 15:30:19 +0200

    [FX-649] Backported Mangos 10081
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/shared/Config/Config.cpp
+++ src/shared/Config/Config.cpp
@@ -1,28 +1,51 @@
 /*
- * Copyright (C) 2005-2010 MaNGOS <http://getmangos.com/>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+* Copyright (C) 2005-2010 MaNGOS <http://getmangos.com/>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
 
 #include "ConfigEnv.h"
+#include "ace/Configuration_Import_Export.h"
+
 #include "Policies/SingletonImp.h"
 
 INSTANTIATE_SINGLETON_1(Config);
 
+static bool GetValueHelper(ACE_Configuration_Heap *mConf, const char *name, ACE_TString &result)
+{
+    if (!mConf)
+        return false;
+
+    ACE_TString section_name;
+    ACE_Configuration_Section_Key section_key;
+    ACE_Configuration_Section_Key root_key = mConf->root_section();
+
+    int i = 0;
+    while (mConf->enumerate_sections(root_key, i, section_name) == 0)
+    {
+        mConf->open_section(root_key, section_name.c_str(), 0, section_key);
+        if (mConf->get_string_value(section_key, name, result) == 0)
+            return true;
+        ++i;
+    }
+
+    return false;
+}
+
 Config::Config()
-: mIgnoreCase(true), mConf(NULL)
+: mConf(NULL)
 {
 }
 
@@ -31,9 +54,8 @@ Config::~Config()
     delete mConf;
 }
 
-bool Config::SetSource(const char *file, bool ignorecase)
+bool Config::SetSource(const char *file)
 {
-    mIgnoreCase = ignorecase;
     mFilename = file;
 
     return Reload();
@@ -42,43 +64,33 @@ bool Config::SetSource(const char *file, bool ignorecase)
 bool Config::Reload()
 {
     delete mConf;
+    mConf = new ACE_Configuration_Heap;
 
-    mConf = new DOTCONFDocument(mIgnoreCase ?
-        DOTCONFDocument::CASEINSENSETIVE :
-    DOTCONFDocument::CASESENSITIVE);
-
-    if (mConf->setContent(mFilename.c_str()) == -1)
+    if (mConf->open() == 0)
     {
-        delete mConf;
-        mConf = NULL;
-        return false;
+        ACE_Ini_ImpExp config_importer(*mConf);
+        if (config_importer.import_config(mFilename.c_str()) == 0)
+            return true;
     }
 
-    return true;
+    delete mConf;
+    mConf = NULL;
+    return false;
 }
 
 std::string Config::GetStringDefault(const char* name, const char* def)
 {
-    if (!mConf)
-        return std::string(def);
-
-    DOTCONFDocumentNode const *node = mConf->findNode(name);
-    if (!node || !node->getValue())
-        return std::string(def);
-
-    return std::string(node->getValue());
+    ACE_TString val;
+    return GetValueHelper(mConf, name, val) ? val.c_str() : def;
 }
 
 bool Config::GetBoolDefault(const char* name, bool def)
 {
-    if (!mConf)
+    ACE_TString val;
+    if (!GetValueHelper(mConf, name, val))
         return def;
 
-    DOTCONFDocumentNode const *node = mConf->findNode(name);
-    if (!node || !node->getValue())
-        return def;
-
-    const char* str = node->getValue();
+    const char* str = val.c_str();
     if (strcmp(str, "true") == 0 || strcmp(str, "TRUE") == 0 ||
         strcmp(str, "yes") == 0 || strcmp(str, "YES") == 0 ||
         strcmp(str, "1") == 0)
@@ -87,28 +99,14 @@ bool Config::GetBoolDefault(const char* name, bool def)
         return false;
 }
 
-
 int32 Config::GetIntDefault(const char* name, int32 def)
 {
-    if (!mConf)
-        return def;
-
-    DOTCONFDocumentNode const *node = mConf->findNode(name);
-    if (!node || !node->getValue())
-        return def;
-
-    return atoi(node->getValue());
+    ACE_TString val;
+    return GetValueHelper(mConf, name, val) ? atoi(val.c_str()) : def;
 }
-
 
 float Config::GetFloatDefault(const char* name, float def)
 {
-    if (!mConf)
-        return def;
-
-    DOTCONFDocumentNode const *node = mConf->findNode(name);
-    if (!node || !node->getValue())
-        return def;
-
-    return atof(node->getValue());
+    ACE_TString val;
+    return GetValueHelper(mConf, name, val) ? (float)atof(val.c_str()) : def;
 }
