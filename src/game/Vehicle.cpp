54ed4f10c805ac34146358a3b2c768df802e587b
Ner'zhul <nerzhultheking@gmail.com>
2010-06-30 19:02:03 +0200

    [FX-846] Vehicle mustnt be active if they're not hostile
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/game/Vehicle.cpp
+++ src/game/Vehicle.cpp
@@ -35,7 +35,7 @@ me(unit), m_vehicleInfo(vehInfo),m_vRegenTimer(4000)
     m_updateFlag = (UPDATEFLAG_LIVING | UPDATEFLAG_HAS_POSITION | UPDATEFLAG_VEHICLE);
 	m_duration = DAY * IN_MILLISECONDS * 7;
     InitSeats();
-	if(me->GetTypeId() == TYPEID_PLAYER)
+	if(me->GetTypeId() == TYPEID_UNIT)
 		if(!((Creature*)me)->isHostileVehicle())
 			((Creature*)me)->SetReactState(REACT_PASSIVE);
 }
