5ad59fe205a714ed74319f666f89f7464fff6034
Ner'zhul <nerzhultheking@gmail.com>
2010-07-04 19:08:10 +0200

    [FX-866] Compile fixes for GetObjectScale
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/game/Object.h
+++ src/game/Object.h
@@ -39,6 +39,8 @@
 #define DEFAULT_VISIBILITY_BGARENAS 180.0f      // default visible distance in BG/Arenas, 180 yards
 
 #define DEFAULT_WORLD_OBJECT_SIZE   0.388999998569489f      // player size, also currently used (correctly?) for any non Unit world objects
+#define DEFAULT_OBJECT_SCALE        1.0f                    // player/item scale as default, npc/go from database, pets from dbc
+
 #define MAX_STEALTH_DETECT_RANGE    45.0f
 
 enum TypeMask
@@ -143,6 +145,11 @@ class MANGOS_DLL_SPEC Object
         const ByteBuffer& GetPackGUID() const { return m_PackGUID; }
         uint32 GetEntry() const { return GetUInt32Value(OBJECT_FIELD_ENTRY); }
         void SetEntry(uint32 entry) { SetUInt32Value(OBJECT_FIELD_ENTRY, entry); }
+
+		float GetObjectScale() const
+        {
+            return m_floatValues[OBJECT_FIELD_SCALE_X] ? m_floatValues[OBJECT_FIELD_SCALE_X] : DEFAULT_OBJECT_SCALE;
+        }
 
         uint8 GetTypeId() const { return m_objectTypeId; }
         bool isType(uint16 mask) const { return (mask & m_objectType); }
