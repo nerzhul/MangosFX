eb551af9b61ff6f8d9bcba75fe92c2ac601a8eb9
Ner'zhul <nerzhultheking@gmail.com>
2010-07-19 18:47:30 +0200

    [FX-1013] Added some stuff on Basic Cluster, need test on Linux$
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/Cluster-Basic/CORBAThread.cpp
+++ src/Cluster-Basic/CORBAThread.cpp
@@ -1,12 +1,26 @@
 #include <Policies/SingletonImp.h>
-#include <omniORB4/CORBA.h>
 #include <Log.h>
 #include <Timer.h>
 #include "CORBAThread.h"
+#include "FirstCallBack.hh"
 
 INSTANTIATE_SINGLETON_1( CORBAThread );
 volatile bool CORBAThread::m_stopEvent = false;
 uint8 CORBAThread::m_ExitCode = 0;
+
+class FirstCallBack_i : public POA_FirstCallBack
+{
+	public:
+	  inline FirstCallBack_i() {}
+	  virtual ~FirstCallBack_i() {}
+	  virtual char* nerzhulCB(const char* mesg);
+};
+
+
+char* FirstCallBack_i::nerzhulCB(const char* mesg)
+{
+  return CORBA::string_dup(mesg);
+}
 
 CORBAThread::CORBAThread()
 {
@@ -24,15 +38,37 @@ void CORBAThread::run()
 
 	int argc = 0;
 	char** argv = NULL;
+	CosNaming::NamingContext_var rootContext;
 	try
 	{
 		sLog.outBasic("Initializing CORBA...");
 		CORBA::ORB_ptr orb = CORBA::ORB_init(argc,argv,"omniORB4");
 
 		sLog.outBasic("Initializing CORBA POA...");
-		CORBA::Object_var obj = orb->resolve_initial_references("RootPOA");
+		CORBA::Object_var obj = orb->resolve_initial_references("NameService");
+		rootContext = CosNaming::NamingContext::_narrow(obj);
 		PortableServer::POA_var poa = PortableServer::POA::_narrow(obj);
+		if( CORBA::is_nil(rootContext) ) {
+			sLog.outError("RootContext configuration failed !");
+		}
 
+		sLog.outBasic("Prepare FirstCallBack object...");
+		FirstCallBack_var v = FirstCallBack::_narrow(obj);
+
+		FirstCallBack_i* FCB = new FirstCallBack_i();
+
+		// Activate the object.  This tells the POA that this object is
+		// ready to accept requests.
+		PortableServer::ObjectId_var FCBid = poa->activate_object(FCB);
+
+		// Obtain a reference to the object.
+		FirstCallBack_var FCBref = FCB->_this();
+
+		// Decrement the reference count of the object implementation, so
+		// that it will be properly cleaned up when the POA has determined
+		// that it is no longer needed.
+		//FCB->_remove_ref();
+		
 		/** TODO : activate objects there
 
 		*/
@@ -40,6 +76,15 @@ void CORBAThread::run()
 		sLog.outBasic("Activate CORBA POA Manager...");
 		PortableServer::POAManager_var pman = poa->the_POAManager();
 		pman->activate();
+
+		if(CORBA::is_nil(v))
+			sLog.outError("Cannot Initialize FirstCallBack object");
+		else
+		{
+			CORBA::String_var src = (const char*) "Toto";
+			CORBA::String_var dest = v->nerzhulCB(src);
+			sLog.outError("String recv %s",dest);
+		}
 
 		sLog.outBasic("CORBA Thread launched successfuly !");
 		while(!MustStop())
