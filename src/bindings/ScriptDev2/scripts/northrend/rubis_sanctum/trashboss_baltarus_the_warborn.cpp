3e4665c8d116411b17b9a960d4023871d2f30153
Ner'zhul <nerzhultheking@gmail.com>
2010-08-19 13:51:53 +0200

    [FX-1181] Baltharus at Rubis Sanctum is now scripted
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/bindings/ScriptDev2/scripts/northrend/rubis_sanctum/trashboss_baltarus_the_warborn.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/rubis_sanctum/trashboss_baltarus_the_warborn.cpp
@@ -3,7 +3,12 @@
 
 enum
 {
-
+	SPELL_BLADE_TEMPEST		= 75125,
+	SPELL_REPELLING_WAVE	= 74509,
+	SPELL_ENERVATING_BRAND	= 74502,
+	SPELL_ENERVATING_B_TRIG = 74505,
+	SPELL_SIPHONED_MIGHT	= 74507,
+	SPELL_SUMMON_CLONE		= 74511,
 };
 
 struct MANGOS_DLL_DECL trashboss_baltarusAI : public LibDevFSAI
@@ -11,17 +16,52 @@ struct MANGOS_DLL_DECL trashboss_baltarusAI : public LibDevFSAI
 	trashboss_baltarusAI(Creature* pCreature) : LibDevFSAI(pCreature)
     {	
         InitInstance();
+		AddEventOnTank(SPELL_BLADE_TEMPEST, urand(3000,5000),15000,5000);
+		AddEventOnTank(SPELL_REPELLING_WAVE,urand(7000,9000),12000,1000);
+		AddEvent(SPELL_ENERVATING_BRAND,urand(30000,50000),45000);
     }
 
     void Reset()
 	{
 		ResetTimers();
+		me->RemoveAurasDueToSpell(SPELL_SIPHONED_MIGHT);
+	}
+
+	void SpellHitTarget(Unit* pWho, const SpellEntry* spell)
+	{
+		if(spell->Id == SPELL_ENERVATING_B_TRIG)
+			ModifyAuraStack(SPELL_SIPHONED_MIGHT,1);
+	}
+
+	void JustDied(Unit* pWho)
+	{
+		switch(m_difficulty)
+		{
+			case RAID_DIFFICULTY_10MAN_NORMAL:
+				GiveEmblemsToGroup(GIVRE,1);
+				break;
+			case RAID_DIFFICULTY_25MAN_NORMAL:
+				GiveEmblemsToGroup(GIVRE,1);
+				break;
+			case RAID_DIFFICULTY_10MAN_HEROIC:
+				GiveEmblemsToGroup(GIVRE,2);
+				break;
+			case RAID_DIFFICULTY_25MAN_HEROIC:
+				GiveEmblemsToGroup(GIVRE,2);
+				break;
+		}
 	}
 	
     void UpdateAI(const uint32 diff)
 	{	
         if (!CanDoSomething())
             return;
+
+		if(CheckPercentLife(50))
+		{
+			me->CastStop();
+			DoCastMe(SPELL_SUMMON_CLONE);
+		}
 
 		UpdateEvent(diff);
 	
@@ -33,7 +73,46 @@ struct MANGOS_DLL_DECL trashboss_baltarusAI : public LibDevFSAI
 CreatureAI* GetAI_trashboss_baltarus(Creature* pCreature)
 {
     return new trashboss_baltarusAI(pCreature);
-} 
+}
+
+struct MANGOS_DLL_DECL trashboss_baltarus_cloneAI : public LibDevFSAI
+{	
+	trashboss_baltarus_cloneAI(Creature* pCreature) : LibDevFSAI(pCreature)
+    {	
+        InitInstance();
+		AddEventOnTank(SPELL_BLADE_TEMPEST, urand(3000,5000),15000,5000);
+		AddEventOnTank(SPELL_REPELLING_WAVE,urand(7000,9000),12000,1000);
+		AddEvent(SPELL_ENERVATING_BRAND,urand(30000,50000),45000);
+    }
+
+    void Reset()
+	{
+		ResetTimers();
+		me->RemoveAurasDueToSpell(SPELL_SIPHONED_MIGHT);
+		me->SetHealth(me->GetMaxHealth() / 2);
+	}
+
+	void SpellHitTarget(Unit* pWho, const SpellEntry* spell)
+	{
+		if(spell->Id == SPELL_ENERVATING_B_TRIG)
+			ModifyAuraStack(SPELL_SIPHONED_MIGHT,1);
+	}
+	
+    void UpdateAI(const uint32 diff)
+	{	
+        if (!CanDoSomething())
+            return;
+
+		UpdateEvent(diff);
+		DoMeleeAttackIfReady();
+
+	}
+};
+
+CreatureAI* GetAI_trashboss_baltarus_clone(Creature* pCreature)
+{
+    return new trashboss_baltarus_cloneAI(pCreature);
+}
 
 void AddSC_trashboss_baltarus()
 {
@@ -42,6 +121,11 @@ void AddSC_trashboss_baltarus()
     newscript = new Script;
     newscript->Name = "trashboss_baltarus";
     newscript->GetAI = &GetAI_trashboss_baltarus;
+    newscript->RegisterSelf();
+
+	newscript = new Script;
+    newscript->Name = "trashboss_baltarus_clone";
+    newscript->GetAI = &GetAI_trashboss_baltarus_clone;
     newscript->RegisterSelf();
 }
 
