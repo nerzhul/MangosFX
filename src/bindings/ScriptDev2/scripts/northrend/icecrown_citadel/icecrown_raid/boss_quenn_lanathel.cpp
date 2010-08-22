9fa02e4dc738378d4aa56f0c5cfdbb6717168009
Ner'zhul <nerzhultheking@gmail.com>
2010-08-22 16:59:03 +0200

    [FX-1200] Lanathel fixes
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/bindings/ScriptDev2/scripts/northrend/icecrown_citadel/icecrown_raid/boss_quenn_lanathel.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/icecrown_citadel/icecrown_raid/boss_quenn_lanathel.cpp
@@ -11,8 +11,9 @@ enum BossSpells
 	SPELL_ESSENCE_OF_BLOOD_QWEEN            = 70867,
 	SPELL_FRENZIED_BLOODTHIRST              = 70877,
 	SPELL_UNCONTROLLABLE_FRENZY             = 70923,
-	SPELL_PACT_OF_DARKFALLEN                = 71340,
-	SPELL_SWARMING_SHADOWS                  = 71265,
+	SPELL_PACT_OF_DARKFALLEN                = 71340, // ok: TODO : distance damages
+	SPELL_SWARMING_SHADOWS                  = 71264, // ok: TODO : spawn event and casting region
+	SPELL_FEAR								= 73070, // ok
 	SPELL_TWILIGHT_BLOODBOLT                = 71446,
 	SPELL_BLOODBOLT_WHIRL                   = 71772,
 	SPELL_PRESENCE_OF_DARKFALLEN            = 71952,
@@ -20,11 +21,12 @@ enum BossSpells
 
 enum phases
 {
-	PHASE_LAND			= 0,
-	PHASE_AIR			= 1,
-	SUBPHASE_FEAR		= 0,
-	SUBPHASE_STORM		= 1,
-	SUBPHASE_LANDING	= 2,
+	PHASE_LAND			= 1,
+	PHASE_AIR			= 2,
+	SUBPHASE_MOVE		= 0,
+	SUBPHASE_FEAR		= 1,
+	SUBPHASE_STORM		= 2,
+	SUBPHASE_LANDING	= 3,
 };
 
 struct MANGOS_DLL_DECL boss_lanathelAI : public LibDevFSAI
@@ -33,22 +35,37 @@ struct MANGOS_DLL_DECL boss_lanathelAI : public LibDevFSAI
     {
         InitInstance();
 		AddEventOnTank(SPELL_DELRIOUS_SLASH,3000,20000,0,PHASE_LAND);
-
+		AddEvent(SPELL_SWARMING_SHADOWS,35000,30000,0,TARGET_RANDOM,PHASE_LAND);
 		AddEnrageTimer(320000);
 		AddTextEvent(16793,"On arrete MAINTENANT !",320000,60000);
+		pactTargets = 0;
+		switch(m_difficulty)
+		{
+			case RAID_DIFFICULTY_10MAN_NORMAL:
+			case RAID_DIFFICULTY_10MAN_HEROIC:
+				pactTargets = 2;
+				break;
+			case RAID_DIFFICULTY_25MAN_NORMAL:
+			case RAID_DIFFICULTY_25MAN_HEROIC:
+				pactTargets = 3;
+				break;
+		}
     }
 
 	uint8 phase;
 	uint32 phase_Timer;
 	uint8 subphase;
+	uint32 pact_Timer;
+	uint8 pactTargets;
 
     void Reset()
     {
 		ResetTimers();
 		phase = PHASE_LAND;
 		phase_Timer = 120000;
-		subphase = SUBPHASE_FEAR;
+		subphase = SUBPHASE_MOVE;
 		ModifyAuraStack(SPELL_SHROUD_OF_SORROW);
+		pact_Timer = 20000;
     }
 
     void Aggro(Unit* pWho)
@@ -114,11 +131,17 @@ struct MANGOS_DLL_DECL boss_lanathelAI : public LibDevFSAI
 			}
 			else if(phase == PHASE_AIR)
 			{
-				if(subphase == SUBPHASE_FEAR)
+				if(subphase == SUBPHASE_MOVE)
 				{
-					; // Todo : cast fear
+					me->GetMotionMaster()->MovePoint(0,4595.9f,2769.32f,400.14f);
+					subphase = SUBPHASE_FEAR;
+					phase_Timer = 2000;
+				}
+				else if(subphase == SUBPHASE_FEAR)
+				{
+					DoCastVictim(SPELL_FEAR);					
 					subphase = SUBPHASE_STORM;
-					phase_Timer = 3000;
+					phase_Timer = 2000;
 				}
 				else if(subphase == SUBPHASE_STORM)
 				{
@@ -138,8 +161,32 @@ struct MANGOS_DLL_DECL boss_lanathelAI : public LibDevFSAI
 			phase_Timer -= diff;
 
 		UpdateEvent(diff);
+		UpdateEvent(diff,phase);
 		if(phase == PHASE_LAND)
+		{
+			if(pact_Timer <= diff)
+			{
+				uint8 breaker = 0;
+				for(int8 i=0;i<pactTargets;i++)
+				{
+					if(Unit* u = GetRandomUnit())
+					{
+						if(u->HasAura(SPELL_PACT_OF_DARKFALLEN))
+							i--;
+						else
+							DoCast(u,SPELL_PACT_OF_DARKFALLEN,true);
+					}
+					breaker++;
+					if(breaker >= 50)
+						break;
+				}
+				pact_Timer = 30000;
+			}
+			else
+				pact_Timer -= diff;
+
 			DoMeleeAttackIfReady();
+		}
     }
 };
 
