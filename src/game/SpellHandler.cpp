662a675fcbd1ce0c0e0b23fbb09faab0373542d3
Ner'zhul <nerzhultheking@gmail.com>
2010-08-23 19:51:48 +0200

    [FX-1207] Crashfix
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/game/SpellHandler.cpp
+++ src/game/SpellHandler.cpp
@@ -336,7 +336,7 @@ void WorldSession::HandleCastSpellOpcode(WorldPacket& recvPacket)
     else
     {
         // not have spell in spellbook or spell passive and not casted by client
-        if (!((Creature*)mover)->HasSpell(spellId) || IsPassiveSpell(spellInfo) )
+		if (!(mover->GetTypeId() == TYPEID_UNIT && ((Creature*)mover)->HasSpell(spellId)) || IsPassiveSpell(spellInfo) )
         {
             //cheater? kick? ban?
             recvPacket.rpos(recvPacket.wpos());                 // prevent spam at ignore packet
