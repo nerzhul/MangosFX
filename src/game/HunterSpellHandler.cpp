3527d74347e2de1b506e0e66d6644d21118c1461
Ner'zhul <nerzhultheking@gmail.com>
2010-09-02 20:46:44 +0200

    [FX-1302] Steady shot glyph fix
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/game/HunterSpellHandler.cpp
+++ src/game/HunterSpellHandler.cpp
@@ -5,9 +5,12 @@
 
 INSTANTIATE_SINGLETON_1(HunterSpellHandler);
 
-#define	FLAGS_KILL_SHOT	UI64LIT(0x80000000000000)
-#define FLAG_AIMED_SHOT UI64LIT(0x20000)
-#define FLAG_MULTI_SHOT UI64LIT(0x1000)
+#define	FLAGS_KILL_SHOT			UI64LIT(0x80000000000000)
+#define FLAG_STEADY_SHOT		UI64LIT(0x00000100000000)
+#define FLAG_AIMED_SHOT			UI64LIT(0x00000000020000)
+#define FLAG_VOLLEY				UI64LIT(0x00000000002000)
+#define FLAG_MULTI_SHOT			UI64LIT(0x00000000001000)
+#define FLAG_ARCANE_SHOT		UI64LIT(0x00000000000800)
 
 void HunterSpellHandler::HandleEffectWeaponDamage(Spell* spell, int32 &spell_bonus, bool &weaponDmgMod, float &totalDmgPctMod)
 {
@@ -57,20 +60,23 @@ void HunterSpellHandler::HandleSchoolDmg(Spell *spell,int32 &damage,SpellEffectI
         damage += int32(m_caster->GetTotalAttackPowerValue(BASE_ATTACK)*0.2f);
     }
     // Arcane Shot
-    else if ((m_spellInfo->SpellFamilyFlags & UI64LIT(0x00000800)) && m_spellInfo->maxLevel > 0)
+    else if ((m_spellInfo->SpellFamilyFlags & FLAG_ARCANE_SHOT) && m_spellInfo->maxLevel > 0)
     {
         damage += int32(m_caster->GetTotalAttackPowerValue(RANGED_ATTACK)*0.15f);
     }
 	// Volley
-	else if (m_spellInfo->SpellFamilyFlags & UI64LIT(0x00002000))
+	else if (m_spellInfo->SpellFamilyFlags & FLAG_VOLLEY)
 	{
 		damage += int32(m_caster->GetTotalAttackPowerValue(RANGED_ATTACK)*0.084f);
 	}
     // Steady Shot
-    else if (m_spellInfo->SpellFamilyFlags & UI64LIT(0x100000000))
+    else if (m_spellInfo->SpellFamilyFlags & FLAG_STEADY_SHOT)
     {
         int32 base = irand((int32)m_caster->GetWeaponDamageRange(RANGED_ATTACK, MINDAMAGE),(int32)m_caster->GetWeaponDamageRange(RANGED_ATTACK, MAXDAMAGE));
         damage += int32(float(base)/m_caster->GetAttackTime(RANGED_ATTACK)*2800 + m_caster->GetTotalAttackPowerValue(RANGED_ATTACK)*0.1f);
+		// Steady Shot glyph
+		if(m_caster->HasAura(56826) && (spell->getUnitTarget()->HasAura(49001) || spell->getUnitTarget()->HasAura(49000)  || spell->getUnitTarget()->HasAura(27016)))
+			damage *= 1.1f;
     }
     // Explosive Trap Effect
     else if (m_spellInfo->SpellFamilyFlags & UI64LIT(0x00000004))
