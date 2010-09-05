f3a779f7631fdfcd9d32b32a8a1fc2254bd7168d
Ner'zhul <nerzhultheking@gmail.com>
2010-09-05 20:40:10 +0200

    typo fix
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -4252,7 +4252,7 @@ void Spell::EffectDispel(uint32 i)
     // Ok if exist some buffs for dispel try dispel it
     if (!dispel_list.empty())
     {
-        std::list <std::pair<uint32,uint64>> success_list;// (spell_id,casterGuid)
+        std::list < std::pair<uint32,uint64> > success_list;// (spell_id,casterGuid)
         std::list <uint32> fail_list;                     // spell_id
 
         // some spells have effect value = 0 and all from its by meaning expect 1
