04a618bf00eb3bc951d8cb522e3746c4a8b4640a
Ner'zhul <nerzhultheking@gmail.com>
2010-10-04 18:58:43 +0200

    [FX-1464] Remove one false crash fix
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -3690,11 +3690,12 @@ void Map::SendObjectUpdates()
 	WorldPacket packet;                                     // here we allocate a std::vector with a size of 0x10000
 	for(UpdateDataMapType::iterator iter = update_players.begin(); iter != update_players.end(); ++iter)
 	{
-		iter->second.BuildPacket(&packet);
-		iter->first->GetSession()->SendPacket(&packet);
+		if(iter->second)
+			iter->second.BuildPacket(&packet);
+		if(iter->first && iter->first->GetSession())
+			iter->first->GetSession()->SendPacket(&packet);
 		packet.clear();                                     // clean the string
 	}
-	update_players.clear();
 }
 
 uint32 Map::GenerateLocalLowGuid(HighGuid guidhigh)
