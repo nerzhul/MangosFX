a56c492887b5905bf66ec48ee843b90b9bf4c5bb
Ner'zhul <nerzhultheking@gmail.com>
2010-10-10 08:29:43 +0200

    [FX-1556] Revision 4
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/game/GameObject.h
+++ src/game/GameObject.h
@@ -25,6 +25,7 @@
 #include "LootMgr.h"
 #include "Database/DatabaseEnv.h"
 #include "OutdoorPvP.h"
+#include "cIncludes.h"
 
 // GCC have alternative #pragma pack(N) syntax and old gcc version not support pack(push,N), also any gcc version not support it at some platform
 #if defined( __GNUC__ )
