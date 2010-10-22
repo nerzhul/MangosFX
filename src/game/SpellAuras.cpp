cd8cea1d92b0dcd4223be44b153373c5ea89c009
Ner'zhul <nerzhultheking@gmail.com>
2010-10-22 07:11:18 +0200

    [FX-1609] Channel fix
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -462,14 +462,13 @@ m_isRemovedOnShapeLost(true), m_in_use(0), m_deleted(false)
     //sLog.outDebug("Aura: construct Spellid : %u, Aura : %u Duration : %d Target : %d Damage : %d", m_spellProto->Id, m_spellProto->EffectApplyAuraName[eff], m_maxduration, m_spellProto->EffectImplicitTargetA[eff],damage);
 
     m_effIndex = eff;
-    //SetModifier(AuraType(m_spellProto->EffectApplyAuraName[eff]), damage, m_spellProto->EffectAmplitude[eff], m_spellProto->EffectMiscValue[eff]);
 
     // Start periodic on next tick or at aura apply
     if (!(m_spellProto->AttributesEx5 & SPELL_ATTR_EX5_START_PERIODIC_AT_APPLY))
     {
-        if(GetSpellProto()->AttributesEx & (SPELL_ATTR_EX_CHANNELED_1 | SPELL_ATTR_EX_CHANNELED_2))
+        /*if(GetSpellProto()->AttributesEx & (SPELL_ATTR_EX_CHANNELED_1 | SPELL_ATTR_EX_CHANNELED_2))
             m_periodicTimer += GetChannelPeriodic();
-        else
+        else*/
             m_periodicTimer += m_modifier.periodictime;
     }
 
