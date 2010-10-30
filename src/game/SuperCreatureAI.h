082b9937810ee5db30049936647592ae557961c2
nerzhul <nerzhultheking@gmail.com>
2010-10-30 15:43:33 +0200

    [CATA-039] Makefile fix, prepare SuperCreatureAI for new IA
    Signed-off-by: nerzhul <nerzhultheking@gmail.com>

---
+++ src/game/SuperCreatureAI.h
@@ -0,0 +1,12 @@
+#ifndef __SUPERCREATUREAI_H_
+#define __SUPERCREATUREAI_H_
+
+#include "Common.h"
+
+class MANGOS_DLL_DECL SuperCreatureAI : public CreatureAI
+{
+	public:
+		~SuperCreatureAI() {}
+};
+
+#endif
