3c0483e040126f2f6b6f4ca9532d99c7c49d70d5
Ner'zhul <nerzhultheking@gmail.com>
2010-11-09 19:06:54 +0100

    Finally merge all
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "4bc5b4d626b3ccaee7d64b162f045da12c469432"
+ #define REVISION_ID "5a3b6984095d431d72f5dcfb707892c1d77a7074"
  #define REVISION_DATE "*"
  #define REVISION_TIME "*"
 #endif // __REVISION_H__
