7947256f283cd7b7083dcf80f5c8a56c6d745efa
Ner'zhul <nerzhultheking@gmail.com>
2010-11-19 00:17:36 +0100

    [FX-1808] Backported Mangos 10724
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "24cc32146ee62346fc31fd3814dbe8863d6f1e35"
- #define REVISION_DATE "2010-11-15"
- #define REVISION_TIME "19:09:01"
+ #define REVISION_ID "04be0fcead47179d9f6d1986b94ca69498dd3956"
+ #define REVISION_DATE "2010-11-18"
+ #define REVISION_TIME "19:56:49"
 #endif // __REVISION_H__
