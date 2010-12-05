95297d52386ff37889c58e3629b238f58f08f27d
Ner'zhul <nerzhultheking@gmail.com>
2010-12-05 11:58:37 +0100

    [CATA-X] Added Waterthrone basic scripts
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define REVISION_ID "9535b7d748b64cb6c61228763dfa663bc317035d"
- #define REVISION_DATE "*"
- #define REVISION_TIME "*"
+ #define REVISION_ID "b547fbdfab6a09a9a5a0a6ca62d5e89ff06cac20"
+ #define REVISION_DATE "2010-12-05"
+ #define REVISION_TIME "02:23:32"
 #endif // __REVISION_H__
