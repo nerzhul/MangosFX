498e2d7229e338a32c26f5fed74e303ecb473995
eloo <mareloange@gmail.com>
2010-12-07 21:58:24 -0300

    Fix Rune Death proc

---
+++ sql/Rune_Death_spell_proc_event.sql
@@ -0,0 +1,9 @@
+
+/* Death Rune Mastery */
+REPLACE INTO `spell_proc_event` VALUES ('50033', '0', '15', '16', '131072', '0', '0', '0', '0', '0', '0');
+REPLACE INTO `spell_proc_event` VALUES ('50034', '0', '15', '16', '131072', '0', '0', '0', '0', '0', '0');
+
+/* Blood of the North */ 
+REPLACE INTO `spell_proc_event` VALUES ('54639', '0', '15', '4194304', '65536', '0', '0', '0', '0', '0', '0');
+REPLACE INTO `spell_proc_event` VALUES ('54638', '0', '15', '4194304', '65536', '0', '0', '0', '0', '0', '0');
+REPLACE INTO `spell_proc_event` VALUES ('54637', '0', '15', '4194304', '65536', '0', '0', '0', '0', '0', '0');
