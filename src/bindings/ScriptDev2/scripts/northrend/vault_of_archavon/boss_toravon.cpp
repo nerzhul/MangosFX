b60fee944a8f3a222d28ba3f604dcb507fab5664
Ner'zhul <nerzhultheking@gmail.com>
2010-12-12 12:06:04 +0100

    [FX-1954] Toravon is now full scripted
    Signed-off-by: Ner'zhul <nerzhultheking@gmail.com>

--- src/bindings/ScriptDev2/scripts/northrend/vault_of_archavon/boss_toravon.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/vault_of_archavon/boss_toravon.cpp
@@ -44,7 +44,6 @@ struct MANGOS_DLL_DECL boss_toravonAI : public LibDevFSAI
 	{
 		if(spell->Id == 72091)
 		{
-			CallCreature(38461,TEN_MINS,PREC_COORDS,AGGRESSIVE_RANDOM,pWho->GetPositionX(),pWho->GetPositionY(),pWho->GetPositionZ());
 			if(m_difficulty == RAID_DIFFICULTY_25MAN_NORMAL)
 			{
 				CallCreature(38461,TEN_MINS,PREC_COORDS,AGGRESSIVE_RANDOM,pWho->GetPositionX(),pWho->GetPositionY(),pWho->GetPositionZ());
