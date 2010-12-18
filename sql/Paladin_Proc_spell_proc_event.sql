1928ea3c9af340a6f2791719fbe866f3fc475948
eloo <mareloange@gmail.com>
2010-12-18 23:16:35 +0900

    Many Paladin procs fix

---
+++ sql/Paladin_Proc_spell_proc_event.sql
@@ -0,0 +1,26 @@
+/* Judgement Light/Justice/Wisdom */
+REPLACE INTO `spell_proc_event` VALUES ('20184', '1', '0', '0', '0', '0', '0', '0', '15', '0', '0');
+REPLACE INTO `spell_proc_event` VALUES ('20185', '1', '0', '0', '0', '0', '0', '0', '15', '0', '0');
+REPLACE INTO `spell_proc_event` VALUES ('20186', '1', '0', '0', '0', '0', '0', '0', '15', '0', '0');
+
+/* Judgement of the Wise */
+REPLACE INTO `spell_proc_event` VALUES ('31876', '0', '10', '8388608', '0', '8', '16656', '0', '0', '0', '0');
+REPLACE INTO `spell_proc_event` VALUES ('31877', '0', '10', '8388608', '0', '8', '16656', '0', '0', '0', '0');
+REPLACE INTO `spell_proc_event` VALUES ('31878', '0', '10', '8388608', '0', '8', '16656', '0', '0', '0', '0');
+
+/* Judgements of the Pure */
+REPLACE INTO `spell_proc_event` VALUES ('53671', '0', '10', '8388608', '8388608', '8388608', '0', '0', '0', '100', '0');
+REPLACE INTO `spell_proc_event` VALUES ('53673', '0', '10', '8388608', '8388608', '8388608', '0', '0', '0', '100', '0');
+REPLACE INTO `spell_proc_event` VALUES ('54151', '0', '10', '8388608', '8388608', '8388608', '0', '0', '0', '100', '0');
+REPLACE INTO `spell_proc_event` VALUES ('54154', '0', '10', '8388608', '8388608', '8388608', '0', '0', '0', '100', '0');
+REPLACE INTO `spell_proc_event` VALUES ('54155', '0', '10', '8388608', '8388608', '8388608', '0', '0', '0', '100', '0');
+
+/* Infusion of Light */
+REPLACE INTO `spell_proc_event` VALUES ('53569', '0', '10', '2097152', '65536', '0', '0', '2', '0', '0', '0');
+REPLACE INTO `spell_proc_event` VALUES ('53576', '0', '10', '2097152', '65536', '0', '0', '2', '0', '0', '0');
+
+/* Sacred Cleansing */
+REPLACE INTO `spell_proc_event` VALUES ('53551', '0', '10', '4096', '0', '0', '0', '0', '0', '0', '0');
+REPLACE INTO `spell_proc_event` VALUES ('53552', '0', '10', '4096', '0', '0', '0', '0', '0', '0', '0');
+REPLACE INTO `spell_proc_event` VALUES ('53553', '0', '10', '4096', '0', '0', '0', '0', '0', '0', '0');
+
