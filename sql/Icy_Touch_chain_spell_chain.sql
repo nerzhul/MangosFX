d792631150b0ff5a46b5c96c5c866ca9eee9e538
eloo <mareloange@gmail.com>
2010-12-18 11:26:37 +0900

    Restore Icy Touch chain spell

---
+++ sql/Icy_Touch_chain_spell_chain.sql
@@ -0,0 +1,10 @@
+/* Restore some Spell chain */
+
+/* Icy Touch */
+DELETE FROM spell_chain WHERE first_spell = 45477
+INSERT INTO spell_chain VALUES(45477,0,45477,1,0);
+INSERT INTO spell_chain VALUES(49896,45477,45477,2,0);
+INSERT INTO spell_chain VALUES(49903,49896,45477,3,0);
+INSERT INTO spell_chain VALUES(49904,49903,45477,4,0);
+INSERT INTO spell_chain VALUES(49909,49909,45477,5,0);
+
